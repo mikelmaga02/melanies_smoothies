dc16c0310546d190e64510d7d8a016cd696bdda6
mikelmaga02 <mikelmaga.02@gmail.com>
2026-03-10 12:24:05 +0000

    Update API call with correct fruit names
    Added 'SEARCH_ON' column to the dataframe and updated API call to use it for fetching fruit information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests 
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -11,7 +12,11 @@ st.write(
 )
 cnx = st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+
+# convert snowpark df to pandas df
+pd_df = my_dataframe.to_pandas()
+#st.dataframe(pd_df)
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
@@ -27,9 +32,11 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")  
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
 
